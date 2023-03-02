44532c37ec355882ebf0f0c2614f13d0d213655b
Matt Hardman <matt@whaleblue.me>
2023-03-02 22:24:59 -0100

    cas

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,121 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const Page = () => {
+  const [result, setResult] = React.useState({
+    loading: false,
+    data: { text: "", url1: "", url2: "" },
+    error: false,
+  });
 
-export default function Home() {
+  const [things, setThings] = React.useState({
+    one: "",
+    two: "",
+  });
+
+  const { one, two } = things;
+
+  const onClick = async () => {
+    setResult({
+      loading: true,
+      data: { text: "", url1: "", url2: "" },
+      error: false,
+    });
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        one,
+        two,
+      }),
+    });
+
+    const data = await response.json();
+
+    setResult({
+      loading: false,
+      data: { text: data.result, url1: data.url1, url2: data.url2 },
+      error: false,
+    });
+
+    if (data.error) {
+      setResult({
+        loading: false,
+        data: { text: "", url1: "", url2: "" },
+        error: true,
+      });
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p className="text-blue-300">
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="flex flex-col space-y-4 justify-center p-4 w-full md:px-64 md:py-4">
+      <h1 className="text-6xl font-bold">THING CHOOSER</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <label htmlFor="two" className="flex flex-col">
+        <small>THING ONE</small>
+        <input
+          type="text"
+          placeholder="Enter a thing..."
+          value={one}
+          className="mt-2 h-12 w-full md:w-96 rounded-lg border-2 border-white bg-transparent overflow-hidden px-4"
+          onChange={(e) => setThings({ ...things, one: e.target.value })}
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      </label>
+      <label htmlFor="two" className="flex flex-col">
+        <small>THING TWO</small>
+        <input
+          type="text"
+          placeholder="Enter a thing another..."
+          value={two}
+          className="mt-2 h-12 w-full md:w-96 rounded-lg border-2 border-white bg-transparent overflow-hidden px-4"
+          onChange={(e) => setThings({ ...things, two: e.target.value })}
+        />
+      </label>
+      <div className="flex flex-col md:flex-row gap-4 w-full md:w-96">
+        <button
+          className="bg-white text-slate-800 rounded-lg py-3 px-6 w-full"
+          onClick={onClick}
+        >
+          {result.loading ? "LOADING..." : "CHOOSE FOR ME"}
+        </button>
+        <button
+          className="border-2 border-white rounded-lg py-3 px-6 text-white w-full"
+          onClick={() => {
+            setThings({ one: "", two: "" });
+            setResult({
+              loading: false,
+              data: { text: "", url1: "", url2: "" },
+              error: false,
+            });
+          }}
+        >
+          RESET
+        </button>
+      </div>
+
+      <hr />
+
+      {result.loading === false && result.data.text && (
+        <div className="p-4 border-2 border-white rounded-lg">
+          {result.data.text
+            .split(`.`)
+            .map(
+              (sentence, i) => !!sentence.length && <p key={i}>{sentence}.</p>
+            )}
         </div>
-      </div>
+      )}
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {result.loading === false && result.data.url1 && result.data.url2 && (
+        <div className="text-white flex flex-col md:flex-row gap-4 p-4 border-2 border-white rounded-lg ">
+          <p>Here are some pictures to help you decide:</p>
+          <img src={result.data.url1} alt="" className="w-96" />
+          <img src={result.data.url2} alt="" className="w-96" />
+        </div>
+      )}
+    </div>
+  );
+};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Page;
